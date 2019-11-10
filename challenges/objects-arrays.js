c6363c25b9417819aab6d0db39d8e4704d8afbfb
Shadewalker1984 <Shadewalker1984@yahoo.com>
2019-11-09 21:47:55 -0600

    random push prior to sleeping

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -103,14 +103,6 @@ for(let i=0; i < graduates.length; i++) {
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
-for(let i=0; i < graduates.length; i++) {
-  graduates.includes('Uni')
-  // switch(graduates[i].university) { 
-  //   case "Uni":
-  //     unisWithUni.push(gradutes[i].university);
-  //     default:
-console.log(unisWithUni);
 
 
 ////COME BACK TO THIS ONE ABOVE///
@@ -162,8 +154,7 @@ The zoos are concerned about animals with a lower population count. Using filter
 
 */
 const lowPopulationAnimals = [];
-zooAnimals.filter(zooAnimal => 
-   lowPopulationAnimals.push((zooAnimals.population < 5))); 
+lowPopulationAnimals.push( zooAnimals.filter((zooAnimals) => zooAnimals.population < 5));
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -171,14 +162,15 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
-zooAnimals.reduce((callback, populationTotal, num) => {
-  return populationTotal + num.population;
+
+
+const populationTotal = zooAnimals.reduce((populationTotal, value) => {
+  return populationTotal + value.population;
 }, 0);
 
 console.log(populationTotal);
 
-}
+
 /*
 
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
